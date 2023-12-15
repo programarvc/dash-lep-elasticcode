b33ac1212241bbbdc4666a9aaa3e5b5b5f73f836
Tiago Santos <tiagojss.santos@gmail.com>
2023-12-15 18:19:52 -0300

    Atualização no DatabaseLoadConfig/ alteração da porta no proxy.conf.ts e application.properties

--- dashboard/proxy.conf.ts
+++ dashboard/proxy.conf.ts
@@ -1,7 +1,7 @@
 const proxy = [
   {
     context: '/api',
-    target: 'http://localhost:8080',
+    target: 'http://localhost:8091',
     pathRewrite: { '^/api': '' },
     secure: false,
     changeOrigin: true,
