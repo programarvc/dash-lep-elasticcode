4dc669b07a458cef2ea4ae3b029394bf036439d2
mavis-martins <mavismr98@gmail.com>
2023-12-22 18:50:30 -0300

    fix: generationType and colaboradorempresaentity table.

--- dash/src/main/java/com/br/agilize/dash/model/entity/AcoesColaboradorEntity.java
+++ dash/src/main/java/com/br/agilize/dash/model/entity/AcoesColaboradorEntity.java
@@ -16,7 +16,7 @@ import lombok.ToString;
 @Entity
 public class AcoesColaboradorEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @ManyToOne
