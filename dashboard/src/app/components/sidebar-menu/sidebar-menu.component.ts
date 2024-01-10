246e327902ed0a03ee7d4f47deb4ecdb74391ab2
monalizaloren <monalizaloren1@gmail.com>
2024-01-10 16:35:57 -0300

    Colocar o link para o dash do dev no menu lateral

--- dashboard/src/app/components/sidebar-menu/sidebar-menu.component.ts
+++ dashboard/src/app/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar-menu',
   templateUrl: './sidebar-menu.component.html',
   styleUrls: ['./sidebar-menu.component.sass']
 })
-export class SidebarMenuComponent implements OnInit {
+export class SidebarMenuComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  abrirComponente(componente: string) {
+    switch (componente) {
+      case 'app-dash-projeto':
+        this.router.navigate(['/app-dash-projeto']);
+        break;
+      case 'app-colaborador':
+        this.router.navigate(['/app-colaborador']);
+        break;
+      default:
+        break;
+    }
   }
-
 }
