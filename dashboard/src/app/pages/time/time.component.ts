6c1e5bf5bbd6b5cb4228104dcd7770766a8a9a69
mavis-martins <mavismr98@gmail.com>
2024-04-26 09:53:36 -0300

    correção de bloco de código comentado indevidamente.

--- dashboard/src/app/pages/time/time.component.ts
+++ dashboard/src/app/pages/time/time.component.ts
@@ -135,7 +135,7 @@ export class TimeComponent implements OnInit {
   };
 
   //variavel com dados para armazenar a quantidade total de prs por colaborador github API
-  /*
+  
   currentPrFromGithub: PrFromGithub = {
     id: 0,
     prAuthor: '',
@@ -153,7 +153,6 @@ export class TimeComponent implements OnInit {
     countPr: 0,
     nome: ''
   };
-  */
 
   public  currentValorIndiceDeSobrevivencia: IndiceDeSobrevivenciaDev = {
     id: 0,
@@ -450,7 +449,7 @@ public selecionarMetaColaborador (id?: number) {
   }
 
   //Metodos para retornar a quantidade de prs em tempo determinado github API
-  /*
+
   //metodo para retornar o total de prs por colaborador
   getPrFromGithubByColaboradorId(colaboradorId: number) {
     this.timeService.getPrCountForColaboradorId(colaboradorId).subscribe((response) => {
@@ -505,7 +504,6 @@ public selecionarMetaColaborador (id?: number) {
     const colaboradorId = this.currentColaborador.id;
     this.getPrCountLast90DaysForColaborador(colaboradorId);
   }
-*/
 
 }
 
