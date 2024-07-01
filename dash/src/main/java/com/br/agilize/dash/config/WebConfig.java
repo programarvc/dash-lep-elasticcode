57fe46c21cf2641ede9e686e1105189883f7c2c3
Tiago <tiagojss.santos@gmail.com>
2024-07-01 12:28:23 -0300

    configuração de CORS

--- dash/src/main/java/com/br/agilize/dash/config/WebConfig.java
+++ dash/src/main/java/com/br/agilize/dash/config/WebConfig.java
@@ -10,10 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-      registry.addMapping("/**")
-              .allowedOrigins("https://dash.elasticcode.com.br") // ou use "*" para permitir todas as origens
-              .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-              .allowedHeaders("*")
-              .allowCredentials(true);
+    registry.addMapping("/**");
   }
 }
