836076af4401f98570788b990bebc49db0100144
Mavis <mavisdeving@gmail.com>
2024-07-02 17:53:16 -0300

    feat: Add getContPromptsByUserEsteiraId method to PromptService
    This commit adds a new method called `getContPromptsByUserEsteiraId` to the `PromptService` class. This method retrieves the count of prompts based on the `userEsteiraId` parameter. It is used to fetch the number of prompts associated with a specific user esteira.

--- dashboard/src/services/prompts/prompts.service.ts
+++ dashboard/src/services/prompts/prompts.service.ts
@@ -26,4 +26,11 @@ export class PromptService {
             .post<any>(url, promptHistory)
             .pipe(catchError(this.handleError<any>('postPromptHistory')));
     }
+
+    getContPromptsByUserEsteiraId(userEsteiraId: any): Observable<any> {
+        const url =`${environment.api}/prompts-history/count/user-esteira/${userEsteiraId}`;
+        return this.http
+            .get<any>(url)
+            .pipe(catchError(this.handleError<any>('getContPromptsByUserEsteiraId')));
+    }
 }
