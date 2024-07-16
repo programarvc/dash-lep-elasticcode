10333b141455232dd067864f92a2e399aa6f02c1
Mavis <mavisdeving@gmail.com>
2024-07-15 21:58:20 -0300

    fix: remoção de espaços vazios

--- dash/src/main/java/com/br/agilize/dash/model/dto/dashboardDto/JiraActivitiesDto.java
+++ dash/src/main/java/com/br/agilize/dash/model/dto/dashboardDto/JiraActivitiesDto.java
@@ -4,29 +4,16 @@ import lombok.Data;
 
 @Data
 public class JiraActivitiesDto {
-
     private Long id;
-
     private String epic;
-
     private String parent;
-
     private String name;
-
     private String priority;
-
     private String sprint;
-
     private String statusDetail;
-
     private String typeDetail;
-
     private String points;
-
     private String createdAt;
-
     private String source;
-
     private String updatedAt;
-
 }
