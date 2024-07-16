23e41ca62c1884cd50ae8c200bea98ae96b65588
Mavis <mavisdeving@gmail.com>
2024-07-15 21:09:04 -0300

    feat: Add List import to UserDto

--- dash/src/main/java/com/br/agilize/dash/model/dto/dashboardDto/UserDto.java
+++ dash/src/main/java/com/br/agilize/dash/model/dto/dashboardDto/UserDto.java
@@ -3,6 +3,8 @@ package com.br.agilize.dash.model.dto.dashboardDto;
 import com.br.agilize.dash.model.dto.ColaboradorDto;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class UserDto {
     private Long id;
