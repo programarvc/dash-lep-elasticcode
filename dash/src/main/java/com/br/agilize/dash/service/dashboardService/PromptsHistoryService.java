874db45e1314fd66370d3159e5b6860cae7b9131
Mavis <mavisdeving@gmail.com>
2024-07-15 21:59:22 -0300

    fix: ajustes para salvar prompt gerado na página do Elastic.AI

--- dash/src/main/java/com/br/agilize/dash/service/dashboardService/PromptsHistoryService.java
+++ dash/src/main/java/com/br/agilize/dash/service/dashboardService/PromptsHistoryService.java
@@ -1,18 +1,17 @@
 package com.br.agilize.dash.service.dashboardService;
 
 import com.br.agilize.dash.exception.DashNotFoundException;
-import com.br.agilize.dash.mapper.dashboardMapper.PromptJiraActivityMapper;
 import com.br.agilize.dash.mapper.dashboardMapper.PromptsHistoryMapper;
 import com.br.agilize.dash.model.dto.dashboardDto.PromptsHistoryDto;
+import com.br.agilize.dash.model.entity.dashboardEntity.JiraActivitiesEntity;
 import com.br.agilize.dash.model.entity.dashboardEntity.PromptsHistoryEntity;
-import com.br.agilize.dash.repository.dashboardRepository.PromptJiraActivityRepository;
+import com.br.agilize.dash.repository.dashboardRepository.JiraActivitiesRepository;
 import com.br.agilize.dash.repository.dashboardRepository.PromptsHistoryRepository;
-
 import com.br.agilize.dash.service.ServiceCrudBase;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -26,15 +25,12 @@ public class PromptsHistoryService extends ServiceCrudBase<PromptsHistoryDto> {
     private PromptsHistoryMapper mapper;
 
     @Autowired
-    private PromptJiraActivityRepository promptJiraActivityRepository;
-
-    @Autowired
-    private PromptJiraActivityMapper promptJiraActivityMapper;
+    private JiraActivitiesRepository jiraActivitiesRepository;
 
     @Override
     public PromptsHistoryDto obterPorId(Long id){
         return this.mapper.modelToDTO(this.repository.findById(id)
-                .orElseThrow(DashNotFoundException::new));
+                .orElseThrow(() -> new DashNotFoundException("Prompt history not found")));
     }
 
     @Override
@@ -45,7 +41,16 @@ public class PromptsHistoryService extends ServiceCrudBase<PromptsHistoryDto> {
 
     @Override
     public PromptsHistoryDto salvar(PromptsHistoryDto payload) {
-        PromptsHistoryEntity promptsHistorySalvo = this.repository.save(this.mapper.dtoToModel(payload));
+        PromptsHistoryEntity entity = this.mapper.dtoToModel(payload);
+
+        // Associar jiraActivity
+        if (payload.getJiraActivity() != null && payload.getJiraActivity().getId() != null) {
+            JiraActivitiesEntity jiraActivity = jiraActivitiesRepository.findById(payload.getJiraActivity().getId())
+                    .orElseThrow(() -> new DashNotFoundException("JiraActivity not found"));
+            entity.setJiraActivity(jiraActivity);
+        }
+
+        PromptsHistoryEntity promptsHistorySalvo = this.repository.save(entity);
         return this.mapper.modelToDTO(promptsHistorySalvo);
     }
 
