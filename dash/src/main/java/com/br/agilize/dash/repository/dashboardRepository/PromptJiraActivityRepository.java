953f535d5034333a5b65ee352a0f0e3879e0f0bb
Mavis <mavisdeving@gmail.com>
2024-07-15 22:13:25 -0300

    rm: removido entidades, dtos, repository e mapper inutilizados

--- dash/src/main/java/com/br/agilize/dash/repository/dashboardRepository/PromptJiraActivityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.br.agilize.dash.repository.dashboardRepository;
-
-import com.br.agilize.dash.model.entity.dashboardEntity.PromptJiraActivityEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PromptJiraActivityRepository extends JpaRepository<PromptJiraActivityEntity, Long>{
-
-}
