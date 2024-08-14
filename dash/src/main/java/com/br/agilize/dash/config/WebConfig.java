f082fc8d2517184f26dc6a8d660794926318acea
Mavis <mavisdeving@gmail.com>
2024-08-14 17:15:22 -0300

    feat: Adicionar configuração de CORS para permitir origem específica, métodos, cabeçalhos e envio de credenciais

--- dash/src/main/java/com/br/agilize/dash/config/WebConfig.java
+++ dash/src/main/java/com/br/agilize/dash/config/WebConfig.java
@@ -8,8 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**");
+    registry.addMapping("/**")
+            .allowedOrigins("https://dash.elasticcode.com.br")  // Apenas permitir esta origem
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
+            .allowedHeaders("*")  // Cabeçalhos permitidos
+            .allowCredentials(true);  // Permitir envio de credenciais (cookies, headers de autorização)
   }
 }
